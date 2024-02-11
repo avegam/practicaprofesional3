1f81b5c90e981d6d0697c0d830fd281a2a8462a0
franco-alvarez23 <franco.alvarez2612@gmail.com>
2024-02-11 20:50:08 -0300

    validaciones Js edicion productos

--- client/js/validarEdicionProductos.js
+++ client/js/validarEdicionProductos.js
@@ -1,52 +1,121 @@
-function Validacion() {
-    
-    nombre = document.getElementById("productoanadir").value;
-    ingredientes= document.getElementById("ingredientesAgregar").value;
-    uso = document.getElementById("usoagregar").value;
-    
 
-    
+///////////////////////////////////////VALIDACIONES DE AGREGAR PRODUCTOS////////////////////////////////////////////////////////////////////////////////////////
+function validacion() {
+              var nombreProducto = document.getElementById("productoanadir").value;
+              var imagen = document.getElementById("imagenagregar").value;
+              var precio = document.getElementById("valorAgregrar").value;
+              var stock = document.getElementById("existenciaAgregrar").value;
+              var ingredientes = document.getElementById("ingredientesAgregar").value;
+              var uso = document.getElementById("usoagregar").value;
 
-    // Expresion  Regular Nombre del producto
+               // Expresion  Regular Campos script
+
+               expresion = /^[a-zA-Z]+$/;
+
+              if (nombreProducto === "") {
+                  alert("Por favor, ingrese un nombre de producto");
+                  return false;
+              }
+              else if (!expresion.test(nombreProducto)) {
+              window.alert("Error: Campo Invalido");
+              return false;
+      }
+
+              if (imagen === "") {
+                  alert("Por favor, ingrese una imagen");
+                  return false;
+              }
+
+              if (precio === "") {
+                  alert("Por favor, ingrese un precio");
+                  return false;
+              }
+
+              if (stock === "") {
+                  alert("Por favor, ingrese una cantidad de stock");
+                  return false;
+              }
+
+              if (ingredientes === "") {
+                  alert("Por favor, ingrese los ingredientes");
+                  return false;
+              }
+              else if (!expresion.test(ingredientes)) {
+            window.alert("Error: Campo Invalido");
+            return false;
+      }
+
+              if (uso === "") {
+                  alert("Por favor, ingrese el uso del producto");
+                  return false;
+              }
+              else if (!expresion.test(uso)) {
+            window.alert("Error: Campo Invalido");
+            return false;
+      }
+
+              if (precio === "") {
+                  alert("Por favor, ingrese un precio");
+                  return false;
+              }
+
+              return true;
+          }
+
+/////////////////////////////VALIDACIONES DE EDITAR PRODUCTOS//////////////////////////////////////////////////////////////////////////////////////////////
+          function validar() {
+          var nombreProducto = document.getElementById("productoEditar").value;
+          var imagen = document.getElementById("imagenEditar").value;
+          var precio = document.getElementById("valorEditar").value;
+          var stock = document.getElementById("stockEditar").value;
+          var ingredientes = document.getElementById("ingredientesEditar").value;
+          var uso = document.getElementById("usoEditar").value;
+
+          
+    // Expresion  Regular Campos String
 
     expresion = /^[a-zA-Z]+$/;
 
-   
-    // Validacion Campo Nombre del producto:
-    if (nombre == "") {
-        window.alert("El campo nombre no puede estar vacio");
-        return false;
-    }
-    else if (!expresion.test(nombre)) {
-        window.alert("Error: Nombre Invalido");
-        return false;
-    }
+          if (nombreProducto === "") {
+              alert("Por favor, elija el producto disponible");
+              return false;
+          }
+         
+          if (imagen === "") {
+              alert("Por favor elija una imagen de producto");
+              return false;
+          }
 
- // Validacion Campo ingredientes
+          if (precio === "") {
+              alert("Por favor, ingresa el precio");
+              return false;
+          }
 
- if (ingredientes == "") {
-    window.alert("El campo ingrediente no puede estar vacio");
-    return false;
-}
-if (!expresion.test(ingredientes)) {
-    window.alert("Error: Formato invalido");
-    return false;
-}
+          if (stock === "") {
+              alert("Por favor, ingrese una cantidad de stock");
+              return false;
+          }
 
-
- // Validacion Campo uso
-
- if (uso == "") {
-    window.alert("El campo uso no puede estar vacio");
-    return false;
-}
-if (!expresion.test(uso)) {
-    window.alert("Error: Formato invalido");
-    return false;
-}
+          if (ingredientes === "") {
+              alert("Por favor, ingrese los ingredientes");
+              return false;
+          }
+          else if (!expresion.test(ingredientes)) {
+          window.alert("Error: Campo Invalido");
+          return false;
+      }
 
 
-}
+          if (uso === "") {
+              alert("Por favor, ingrese el uso del producto");
+              return false;
+          }
+          else if (!expresion.test(uso)) {
+          window.alert("Error: Campo Invalido");
+          return false;
+      }
 
+          return true;
+      }
 
-    
+  
