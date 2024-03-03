a72536f64f5f0507c39effea211b409573df2029
franco-alvarez23 <franco.alvarez2612@gmail.com>
2024-03-03 10:11:05 -0300

    estilos aplicados en todas las paginas.

--- client/js/DropdownLogueo.js
+++ client/js/DropdownLogueo.js
@@ -5,12 +5,12 @@
     // Mostrar el menú desplegable al pasar el cursor sobre el texto del usuario
     dropdown.addEventListener('mouseover', function() {
         var dropdownContent = document.getElementById('dropdownContent');
-        dropdownContent.style.display = 'block';
+       dropdownContent.style.display = 'block';
     });
 
     // Ocultar el menú desplegable al sacar el cursor del texto del usuario
     dropdown.addEventListener('mouseout', function() {
         var dropdownContent = document.getElementById('dropdownContent');
-        dropdownContent.style.display = 'none';
+       dropdownContent.style.display = 'none';
     });
  
